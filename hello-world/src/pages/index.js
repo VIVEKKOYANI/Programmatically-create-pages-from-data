0106576b05d96900c24ac9c93ae51bd896c09ebc
vivek <vivek.koyani@gmail.com>
2020-03-19 23:46:09 +0530

    import layout

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import Layout from '../components/layout'
 export default () => (
   <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
